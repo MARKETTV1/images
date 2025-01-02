2fd47b539ad9c59e9676fe81d08a2b9bd5f0caa2
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-03 00:19:43 +0100

    Update openspa-8.4.003.sh

--- openspa-8.4.003.sh
+++ openspa-8.4.003.sh
@@ -1,5 +1,4 @@
 #######################################################
 #######################################################
 
-sh /usr/lib/enigma2/python/Plugins/Extensions/AJPan/eliesat-panel/autoupdate-panel.sh && wget -q "--no-check-certificate" https://gitlab.com/eliesat/softcams/-/raw/main/mohamed-os/oscam.sh -O - | /bin/sh
 wget /media/hdd/images/openspa-8.4.003-vuuno4kse-20241224_usb.zip https://openspa.webhop.info/Descarga%20de%20Im%C3%A1genes/Vuplus/vuuno4kse/openspa-8.4.003-vuuno4kse-20241224_usb.zip
