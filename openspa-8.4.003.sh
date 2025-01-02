19e564aad52624904cd19b248b26338f554f77a9
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-03 00:59:34 +0100

    Update openspa-8.4.003.sh

--- openspa-8.4.003.sh
+++ openspa-8.4.003.sh
@@ -4,9 +4,6 @@
 #######################################################
 echo "welcome to UCLAGON PANEL "
 echo "downloading current openspa image"
-.
-.
-.
 
 wget -O /media/hdd/images/openspa-8.4.003-vuuno4kse-20241224_usb.zip https://openspa.webhop.info/Descarga%20de%20Im%C3%A1genes/Vuplus/vuuno4kse/openspa-8.4.003-vuuno4kse-20241224_usb.zip
 echo " wait a munite "
