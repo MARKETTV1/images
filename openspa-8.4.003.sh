a8fd87ceef8bb57b8fa3526fef8068d8d1b7ec22
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-03 00:43:05 +0100

    Update openspa-8.4.003.sh

--- openspa-8.4.003.sh
+++ openspa-8.4.003.sh
@@ -1,4 +1,8 @@
 #######################################################
+####################OPENSPA-8.4.003####################
+#######################################################
 #######################################################
 
-wget /media/hdd/images/openspa-8.4.003-vuuno4kse-20241224_usb.zip https://openspa.webhop.info/Descarga%20de%20Im%C3%A1genes/Vuplus/vuuno4kse/openspa-8.4.003-vuuno4kse-20241224_usb.zip
+wget /media/hdd/ https://openspa.webhop.info/Descarga%20de%20Im%C3%A1genes/Vuplus/vuuno4kse/openspa-8.4.003-vuuno4kse-20241224_usb.zip
+
+
