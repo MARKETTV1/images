c005476170f2e3fc6db7e8d0fcf416e581e2da55
MARKETTV1 <aliridakarim8@gmail.com>
2025-01-28 21:59:20 +0100

    Update openspa-zero4k.sh

--- openspa-zero4k.sh
+++ openspa-zero4k.sh
@@ -6,7 +6,7 @@ echo "welcome to UCLAGON PANEL "
 echo "downloading current openspa image"
 
 wget -O /media/hdd/images/openspa-8.4.003-vuuno4kse-20241224_usb.zip https://openspa.webhop.info/Descarga%20de%20Im%C3%A1genes/Vuplus/vuuno4kse/openspa-8.4.003-vuuno4kse-20241224_usb.zip
-echo " wait  munite "
+echo " wait a munite "
 echo "The images has been successfully installed!"
 
 #######################################################
